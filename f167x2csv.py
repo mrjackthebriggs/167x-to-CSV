ce3e0c861363d860b3de0a59a2f1bf1e816e88c4
CNS Tech <tech@cabnetsol.com.au>
2026-04-30 12:16:03 +0800

    - removed blank columns - better sorting (for now)

--- f167x2csv.py
+++ f167x2csv.py
@@ -1,6 +1,10 @@
+# written in Python 3.14.4 by Jack Briggs on the 29/04/26
+
 import csv
 import avro.datafile
 import avro.io
+import os
+import re
 
 def flatten_dict(d, parent_key='', sep='_'):
     """
@@ -20,10 +24,24 @@ def flatten_dict(d, parent_key='', sep='_'):
 
         elif isinstance(v, list):
             if k == 'circuits':
-                circuits.extend(v)
-            else:    
                 for val in v:
-                    list_flatten, list_circs = flatten_dict(val, new_key, sep=sep)
+                    if len(val.get('testPoints')) > 0:
+                        for tp in val.get('testPoints'):
+                            tp["tp_name"] = tp.pop("name")
+                            circuits.append(tp)
+                    else:
+                        circuits.append(val)
+            else:    
+                for i,val in enumerate(v):
+
+                    get_name = val.get('name');
+                    get_funct = val.get('function')
+                    id_str = [str(i)]
+                    #if get_name: id_str.append(get_name)
+                    #if get_funct: id_str.append(get_funct)
+                    id = '-'.join(id_str)
+
+                    list_flatten, list_circs = flatten_dict(val, id + '-' + new_key, sep=sep)
                     items.extend(list_flatten.items())
 
                     if len(list_circs) > 0:
@@ -40,12 +58,13 @@ def process_avro_to_csv(avro_path, csv_path):
             for record in reader:
 
                 record = record['data']
-                flat_record,curcs = flatten_dict(record)
+
+                flat_record,curcs = flatten_dict(record, parent_key="")
                 
                 for circ in curcs[0][0]:
                     flat_circ_rec,_ = flatten_dict(circ, parent_key='')
                     circ_record = flat_circ_rec | flat_record 
-                    print("\n\n\naaaaa",circ_record)
+                    # print("\n\n\naaaaa",circ_record)
                     all_flattened_rows.append(circ_record)
         
 
@@ -55,8 +74,24 @@ def process_avro_to_csv(avro_path, csv_path):
 
     # Identify all unique keys for CSV headers
     keys = set().union(*(d.keys() for d in all_flattened_rows))
+
+    # remove all keys that never have a value
+    remove_keys = []
+    for i in keys:
+        key_list = []
+        for j in all_flattened_rows:
+            jget = str(j.get(i))
+            key_list.append(jget == 'None' or jget == '')
+
+        if all(key_list):
+            remove_keys.append(i)
+
+    keys = list({*keys}.difference({*remove_keys}))
+
+    print(f"removed {str(len(remove_keys))} blank key/s")
+
     # Filter out list objects that weren't expanded to keep CSV clean
-    fieldnames = [k for k in keys if not isinstance(all_flattened_rows[0].get(k), list)]
+    fieldnames = sorted([k for k in keys], reverse=True)
 
     # Write to CSV
     with open(csv_path, 'w', newline='', encoding='utf-8') as output_file:
@@ -67,4 +102,11 @@ def process_avro_to_csv(avro_path, csv_path):
     print(f"Successfully created {csv_path} with {len(fieldnames)} columns.")
 
 if __name__ == "__main__":
-    process_avro_to_csv('Test_Tezt.F167x', 'output_report.csv')
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    dir_stuff = os.listdir()
+    f167x_files = [j.string for j in [re.search('.F167(x|X)',i) for i in dir_stuff] if j != None]
+
+    file_names = [i.split('.')[0] for i in f167x_files]
+   
+    for f in file_names:
+        process_avro_to_csv(f + '.F167x', f + '.csv')
